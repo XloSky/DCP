259c088faf06150358e3290669172ed50da04d7a
XloSky <36645360+XloSky@users.noreply.github.com>
2026-03-15 21:34:50 -0500

    Update OUTPUT.js

--- scripts/OUTPUT.js
+++ scripts/OUTPUT.js
@@ -1,10 +1,9 @@
-// DCP v4.4 + DCPTime v1.4.8 Output Modifier
 const modifier = (text) => {
   globalThis.text = text;
 
-  DCP("output");
-  DCPTime("output");
+  if (typeof DCP === "function") DCP("output");
+  if (typeof DCPTime === "function") DCPTime("output");
 
-  return { text: globalThis.text };
-};
-modifier(text);
+  return { text: globalThis.text || " " };
+}
+modifier(text)
